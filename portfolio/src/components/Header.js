975020360ac98c56cd02743021313cc4250ff7ee
Fay Vor <phavorsparks@gmail.com>
2018-08-09 21:26:31 +0100

    Update basic files

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,6 +1,8 @@
-const header = document.querySelector('header .container__header');
+const Header = document.querySelector('.container__header');
 
 // Get HeaderBackgroundImages
 const HeaderBackgroundImages = [
   'bng1', 'bng2', 'bng3', 'bng4', 'bng5'
 ]
+
+export { Header, HeaderBackgroundImages };
---
+++ portfolio/src/components/index.js
@@ -0,0 +1,5 @@
+console.log('App is running');
+
+import { Header, HeaderBackgroundImages as BG } from './Header';
+console.log(Header);
+console.dir(BG);
