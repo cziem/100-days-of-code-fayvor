8b7781dd63fe0daf08d104c40332b5dfe6a87e30
Fay Vor <phavorsparks@gmail.com>
2018-08-10 17:55:51 +0100

    import sass and remove link to css from index.html

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,5 +1,9 @@
 console.log('App is running');
 
 import { Header, HeaderBackgroundImages as BG } from './Header';
+import 'normalize.css/normalize.css';
+import '../styles/main.scss';
+import '../styles/style.sass';
+
 console.log(Header);
 console.dir(BG);
