7506760228aeabd6f18a3a2c8165ec9b5e2a5807
Fay Vor <phavorsparks@gmail.com>
2018-08-10 18:20:43 +0100

    Eazi wont leave me alone... Styling header

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,9 +1,9 @@
 console.log('App is running');
 
-import { Header, HeaderBackgroundImages as BG } from './Header';
 import 'normalize.css/normalize.css';
-import '../styles/main.scss';
-import '../styles/style.sass';
+import '../styles/style.scss';
+import '../styles/main.sass';
+import { Header, HeaderBackgroundImages as BG } from './Header';
 
 console.log(Header);
 console.dir(BG);
