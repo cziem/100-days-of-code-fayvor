e10568ff8889bac2e771f6acd522f1545414dff7
Fay Vor <phavorsparks@gmail.com>
2018-08-13 12:37:02 +0100

    useful update

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -2,7 +2,6 @@ console.log('App is running');
 
 import 'normalize.css/normalize.css';
 import '../styles/style.scss';
-import '../styles/main.sass';
 import { Header, HeaderBackgroundImages as BG } from './Header';
 
 console.log(Header);
