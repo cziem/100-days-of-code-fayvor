3b6e277cd1a39b7acaf0dcf638988081f198ab73
Fay Vor <phavorsparks@gmail.com>
2018-08-13 23:25:22 +0100

    file restructuring

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,8 +1,8 @@
 console.log('App is running');
 
 import 'normalize.css/normalize.css';
-import '../styles/style.scss';
-import { Header, HeaderBackgroundImages as BG } from './Header';
+import './styles/style.scss';
+import { Header, HeaderBackgroundImages as BG } from './components/Header';
 
 console.log(Header);
 console.dir(BG);
