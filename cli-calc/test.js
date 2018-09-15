22eb83a358544143672d65ebd87caa75a23ea247
Fay Vor <phavorsparks@gmail.com>
2018-09-15 23:12:14 +0100

    Update with validateNumber()

--- cli-calc/test.js
+++ cli-calc/test.js
@@ -1,7 +1,5 @@
 const assert = require('assert');
-const { add, substract, divide, multiply } = require('./operations')
-
-if (a === b)
+const { add, substract, divide, multiply, validateNumbers } = require('./operations')
 
 it ('correctly calculates the sum of 1 and 3', () => {
   assert.equal(add(1, 3), 4);
@@ -17,4 +15,17 @@ it ('correctly calculates the product of 12 and 12', () => {
 
 it ('correctly calculates the quotient of 10 and 2', () => {
   assert.equal(divide(10, 2), 5);
+})
+
+// Validator function
+it ('indicates failure when a string is used instead of a number', () => {
+  assert.equal(validateNumbers('phavor', 5), false);
+})
+
+it ('indicates failure when two strings are used instead of numbers', () => {
+  assert.equal(validateNumbers('phavor', 'Fayvor'), false);
+})
+
+it ('successfully runs when two numbers are used', () => {
+  assert.equal(validateNumbers(5, 5), true);
 })
