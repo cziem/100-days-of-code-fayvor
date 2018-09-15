f20e916ba2ecfad23f566cf028beb2b4cc001319
Fay Vor <phavorsparks@gmail.com>
2018-09-15 17:53:39 +0100

    update with basic functionalities

---
+++ cli-calc/operations.js
@@ -0,0 +1,11 @@
+const add = (x, y) => (+x) + (+y);
+const substract = (x, y) => (+x) - (+y);
+const multiply = (x, y) => (+x) * (+y);
+const divide = (x, y) => (+x) / (+y);
+
+module.exports = {
+  add,
+  substract,
+  divide,
+  multiply
+}
