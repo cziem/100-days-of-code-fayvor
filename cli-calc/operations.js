cb2fd7d27d623c9431ad35d336e4297a51a0a97e
Fay Vor <phavorsparks@gmail.com>
2018-09-15 23:12:41 +0100

    Add the validateNumber()

--- cli-calc/operations.js
+++ cli-calc/operations.js
@@ -3,9 +3,17 @@ const substract = (x, y) => (+x) - (+y);
 const multiply = (x, y) => (+x) * (+y);
 const divide = (x, y) => (+x) / (+y);
 
+const validateNumbers = (x, y) => {
+  if (isNaN(x) || isNaN(y)) {
+    return false;
+  }
+  return true
+}
+
 module.exports = {
   add,
   substract,
   divide,
-  multiply
+  multiply,
+  validateNumbers
 }
