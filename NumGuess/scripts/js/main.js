9fbece09f6c14b120413fb48ffa496b188b20ef6
Fay Vor <phavorsparks@gmail.com>
2018-10-04 22:21:47 +0100

    Update Game logic

--- NumGuess/scripts/js/main.js
+++ NumGuess/scripts/js/main.js
@@ -1,1 +1,42 @@
-console.log('app is running')
+let GAME = {
+  score: 0,
+  userNum: 0,
+  isPlaying: false,
+  generateNum() {
+    return Math.floor(Math.random() * 100 + 1)
+  },
+  getUserNumber() {
+    return this.userNum
+  }
+}
+
+// Get DOM Elements
+const startButton = document.querySelector('.start')
+
+// Start Game
+const startGame = () => {
+  GAME.isPlaying = true // Not doing anything yet
+  GAME.userNum = 0
+
+  const number = GAME.generateNum()
+  const userNumber = GAME.getUserNumber()
+  console.log(number)
+
+  if (userNumber === number) {
+    console.log('correct')
+  } else {
+    console.log('Wrong')
+  }
+}
+
+
+// Get user number
+window.addEventListener('keypress', (e) => {
+  if (e.key.match(/\d$/)) {
+    return GAME.userNum += e.key
+  }
+})
+
+// Event listners
+startButton.addEventListener('click', startGame)
+
