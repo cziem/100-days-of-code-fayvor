30df933b3bb8180a706375fdc18cc1a524be2fca
Fay Vor <phavorsparks@gmail.com>
2018-10-06 21:39:51 +0100

    1st iteration

--- NumGuess/scripts/js/main.js
+++ NumGuess/scripts/js/main.js
@@ -1,9 +1,10 @@
 let GAME = {
   score: 0,
-  userNum: 0,
+  userNum: undefined,
   isPlaying: false,
+  generatedNumber: null,
   generateNum() {
-    return Math.floor(Math.random() * 100 + 1)
+    return Math.floor(Math.random() * 10 + 1)
   },
   getUserNumber() {
     return this.userNum
@@ -13,43 +14,43 @@ let GAME = {
 // Get DOM Elements
 // start button
 const startButton = document.querySelector('.start')
-const scoreElement = document.querySelector('.score_box')
+const scoreBoxElement = document.querySelector('.score_box')
+const scoreElement = document.querySelector('.score')
 const displayNumber = document.querySelector('.number_display')
 const feedback = document.querySelector('.feedback')
 
 // Clear the page && Reset
 
-// Show elements
-const showElements = () => {
-  scoreElement.style.display = 'flex'
-}
-
 // Start Game
 const startGame = () => {
-  showElements()
+  scoreBoxElement.style.display = 'flex'
 
   // GAME.isPlaying = true // Not doing anything yet
-  // GAME.userNum = 0
-
-  // const number = GAME.generateNum()
-  // const userNumber = GAME.getUserNumber()
-  // console.log(number)
-
-  // if (userNumber === number) {
-  //   console.log('correct')
-  // } else {
-  //   console.log('Wrong')
-  // }
+  GAME.userNum = ''
+  return GAME.generatedNumber = GAME.generateNum()
 }
-
 
 // Get user number
 window.addEventListener('keypress', (e) => {
   if (e.key.match(/\d$/)) {
-    return GAME.userNum += e.key
+    return GAME.userNum = e.key
   }
+})
+
+// Game Logic 
+window.addEventListener('keyup', () => {
+  const userNumber = GAME.getUserNumber()
+  const number = GAME.generatedNumber
+
+  if (number !== userNumber) {
+    console.log('failed')
+  } else {
+    score += 10
+    scoreElement.textContent = score
+  }
+  
+  console.log('GenNum:'+ number, 'UserNum:' + userNumber)
 })
 
 // Event listners
 startButton.addEventListener('click', startGame)
-
