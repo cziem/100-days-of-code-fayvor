662aeb072f4fdbd6c9c56d287bd5532b840bff07
Fay Vor <phavorsparks@gmail.com>
2018-10-15 17:26:46 +0100

    update server

--- jwt_node_auth/config.js
+++ jwt_node_auth/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   development: {
     port: process.env.PORT || 5000,
+    DB: process.env.MONGODB_URI,
     saltingRounds: 100
   }
 }
--- jwt_node_auth/index.js
+++ jwt_node_auth/index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const logger = require('morgan')
+require('dotenv').config()
 
 const app = express()
 const router = express.Router()
 
 const environment = process.env.NODE_ENV
 const stage = require('./config')[environment]
+const URI = process.env.MONGODB_URI
 
 // Routes
-const routes = require('./routes/index')
+const routes = require('./routes/index.js')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost:27017/100Days_Node_Jwt_Auth', { useNewUrlParser: true })
+  .then(() => console.log('connected to DB'))
+  .catch(err => console.log('Error occurred', err))
+
 
 if (environment !== 'production') {
   app.use(logger('dev'))
