8a4c2a90b3efa304a2e388e656b1c0e8860151bf
Fayvor George <phavorsparks@gmail.com>
2018-11-05 15:57:44 +0100

    Getters and Setters

--- FCC_Solutions/data_structure.js
+++ FCC_Solutions/data_structure.js
@@ -77,3 +77,38 @@ const resultDisplayArray = makeList(result.failure);
  *   `<li class="text-warning">var-on-top</li>`, 
  *   `<li class="text-warning">linebreak</li>` ]
  **/
+
+
+// Getters and Setters
+/**
+ * Getter functions are meant to simply return (get) the value of an object's private variable to the user without    the user directly accessing the private variable.
+
+* Setter functions are meant to modify (set) the value of an object's private variable based on the value passed      into the setter function. This change could involve calculations, or even overwriting the previous value completely.
+**/
+
+function makeClass() {
+"use strict";
+/* Alter code below this line */
+class Thermostat {
+  constructor(temp) {
+    this._temperature = temp;
+  }
+
+  // getter
+  get temperature() {
+    return this._temperature;
+  }
+
+  // setter
+  set temperature(updatedTemp) {
+    this._temperature = updatedTemp
+  }
+}
+/* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
