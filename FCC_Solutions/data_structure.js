f375b2164e81803687e061b8ba0a4265bc334279
Fayvor George <phavorsparks@gmail.com>
2018-11-05 13:45:12 +0100

    add data_structure.js from FCC challenges

---
+++ FCC_Solutions/data_structure.js
@@ -0,0 +1,24 @@
+// use spread operator in fn
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
+
+// Reassignment
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const { tomorrow : tempOfTomorrow } = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
