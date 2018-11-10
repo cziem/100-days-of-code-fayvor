a2207ca162dc01bda9f77db4358f48178949af2f
Fayvor George <phavorsparks@gmail.com>
2018-11-10 23:08:20 +0100

    mutations

--- FCC_Solutions/basic_algorithm_scripting.js
+++ FCC_Solutions/basic_algorithm_scripting.js
@@ -222,3 +222,24 @@ function getIndexToIns(arr, num) {
 }
 
 getIndexToIns([40, 60], 50);
+
+// mutations
+function mutation(arr) {
+  return arr[1].toLowerCase()
+    .split('')
+    .every(function (letter) {
+      return arr[0].toLowerCase()
+        .indexOf(letter) != -1;
+    });
+}
+
+// method 2
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (var i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0)
+      return false;
+  }
+  return true;
+}
