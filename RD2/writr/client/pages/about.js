544ce4f07ecaa69840bf24a5fec6f199ab883b65
Fayvor George <phavorsparks@gmail.com>
2019-08-20 23:06:11 +0100

    create the pages

---
+++ RD2/writr/client/pages/about.js
@@ -0,0 +1,10 @@
+// Import components
+import Layout from '../components/MyLayout';
+
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  );
+}
---
+++ RD2/writr/client/pages/index.js
@@ -0,0 +1,10 @@
+// Import components
+import Layout from '../components/MyLayout';
+
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  );
+}
