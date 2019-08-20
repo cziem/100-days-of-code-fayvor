d01b36794526c99767592404b8f58ca74906d616
Fayvor George <phavorsparks@gmail.com>
2019-08-20 23:37:13 +0100

    dynamic routing basis

--- RD2/writr/client/pages/index.js
+++ RD2/writr/client/pages/index.js
@@ -1,10 +1,34 @@
 // Import components
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const postTitles = [
+  { title: "Hello Next.js" },
+  { title: "Learn awesome Next.js" },
+  { title: "Deploy your next apps with zeit" },
+  { title: "Learn SSR with Next.js" },
+  { title: "What about styling?" },
+  { title: "What Next?" },
+]
+
+const PostLinks = props => (
+  props.map(({ title }, idx) => (
+    <li key={idx}>
+      <Link href={`/post?title=${title}`}>
+        <a>{title}</a>
+      </Link>
+    </li>
+  ))
+)
 
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>Featured Blog Posts</h1>
+
+      <ul>
+        {PostLinks(postTitles)}
+      </ul>
     </Layout>
   );
 }
