8c5c26ab6c480603f8f570a73644e82ad99fba8c
Fayvor George <phavorsparks@gmail.com>
2019-08-21 13:18:49 +0100

    start refactoring

--- RD2/writr/client/pages/index.js
+++ RD2/writr/client/pages/index.js
@@ -1,34 +1,3 @@
-// Import components
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
-
-const postTitles = [
-  { title: "Hello Next.js" },
-  { title: "Learn awesome Next.js" },
-  { title: "Deploy your next apps with zeit" },
-  { title: "Learn SSR with Next.js" },
-  { title: "What about styling?" },
-  { title: "What Next?" },
-]
-
-const PostLinks = props => (
-  props.map(({ title }, idx) => (
-    <li key={idx}>
-      <Link href={`/post?title=${title}`}>
-        <a>{title}</a>
-      </Link>
-    </li>
-  ))
-)
-
-export default function Index() {
-  return (
-    <Layout>
-      <h1>Featured Blog Posts</h1>
-
-      <ul>
-        {PostLinks(postTitles)}
-      </ul>
-    </Layout>
-  );
+export default () => {
+  return 'Hello world'
 }
