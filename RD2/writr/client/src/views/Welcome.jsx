37f0e16fac891b89750b6eefc6d53710d55a1126
Fayvor George <phavorsparks@gmail.com>
2019-08-21 22:10:00 +0100

    finish with Welcome page

--- RD2/writr/client/src/views/Welcome.jsx
+++ RD2/writr/client/src/views/Welcome.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Main, Section, Button } from '../styles/Welcome';
 
 const Welcome = () => (
-	<div>
-		<h1>Welcome to writr</h1>
-	</div>
+	<Main img={require('../assets/img/bng.jpg')}>
+		<div className="welcome">
+			<Section className="welcome__cta">
+				<h2>
+					Hello <span>writr</span>
+				</h2>
+				<p className="welcome__cta-lead">
+					The world can't wait to read what's in your mind
+				</p>
+			</Section>
+			<Section className="welcome__cta">
+				<Button primary>login</Button>
+				<Button fill>sign up</Button>
+			</Section>
+		</div>
+	</Main>
 );
 
 export default Welcome;
