0cc503fc5f5ca884a838fc4495ff0ecdba8758d2
Fayvor George <phavorsparks@gmail.com>
2019-08-22 02:32:24 +0100

    add validations for the sign up details

--- RD2/writr/client/src/helpers/validator.js
+++ RD2/writr/client/src/helpers/validator.js
@@ -6,3 +6,19 @@ export const validateLoginDetails = ({ email, password }) => {
     ? true
     : false;
 };
+
+export const validateSignupDetails = ({ name, username, email, password, roles }) => {
+  const validName = /^([\w]{3,})|[\w]{3,}/g
+  const validUsername = /^[\w]{3,12}/g
+  const validEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/g;
+  const validPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{5,}$/;
+  const validRoles = ['editor', 'writer', 'others']
+
+  return validName.test(name) &&
+    validUsername.test(username) &&
+    validEmail.test(email) &&
+    validPassword.test(password) &&
+    validRoles.indexOf(roles) > 0 ?
+    true :
+    false;
+}
