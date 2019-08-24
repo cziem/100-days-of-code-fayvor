ba3f53128b4fc45f69fb372aac609b6e67326dbe
Fayvor George <phavorsparks@gmail.com>
2019-08-24 23:55:24 +0100

    setup the display picture in drawer mode

--- RD2/writr/client/src/components/shared/Drawer.jsx
+++ RD2/writr/client/src/components/shared/Drawer.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Main } from '../../styles/Drawer';
+import { Main, ProPic } from '../../styles/Drawer';
+import getUser from '../../helpers/getUser';
+
+const token = localStorage.getItem('token');
+const user = getUser(token);
 
 export default ({ width }) => {
 	return (
 		<>
-			<Main width={width}>username</Main>
+			<Main width={width}>
+				<div className="drawer__ppic-wrp">
+					<ProPic pro_pic={require('../../assets/img/dp.jpg')}></ProPic>
+					<p>{user.username}</p>
+				</div>
+			</Main>
 		</>
 	);
 };
