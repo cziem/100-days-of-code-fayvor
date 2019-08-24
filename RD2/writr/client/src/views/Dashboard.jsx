94a7bd0c2e28f300a649dbf488e55e8c8e162517
Fayvor George <phavorsparks@gmail.com>
2019-08-25 00:38:42 +0100

    a bit of a query

--- RD2/writr/client/src/views/Dashboard.jsx
+++ RD2/writr/client/src/views/Dashboard.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import DashboardContent from '../components/DashboardContent';
+import { GET_USER_POSTS, GET_ALL_POSTS } from '../helpers/queries';
+import Loading from '../components/utils/Loading';
+import Error from '../components/utils/Error';
 
-const Dashboard = () => (
-	<DashboardContent>
-		<h4>Here is your dashboard content</h4>
-	</DashboardContent>
-);
+const Dashboard = () => {
+	const { loading, error, data } = useQuery(GET_USER_POSTS);
+
+	if (loading) return <Loading />;
+	if (error) return <Error ErrorText={error.message.split(':').slice(1)} />;
+
+	const { getAllUserPosts: userPosts } = data;
+
+	const renderUserPosts = () => {
+		return userPosts.length > 0
+			? userPosts.map(post => <h3>{post.title}</h3>)
+			: NoPosts;
+	};
+
+	const NoPosts = (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: '100%'
+			}}
+		>
+			<p>You have no posts yet... start writing now</p>
+		</div>
+	);
+
+	console.log('data', userPosts);
+	return <DashboardContent>{renderUserPosts()}</DashboardContent>;
+};
 
 export default Dashboard;
