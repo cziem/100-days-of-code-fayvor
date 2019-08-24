33f8722f8bbd43ed838c7909f7a790b17ada0191
Fayvor George <phavorsparks@gmail.com>
2019-08-24 21:09:10 +0100

    Dashboard design completed

--- RD2/writr/client/src/components/shared/Navbar.jsx
+++ RD2/writr/client/src/components/shared/Navbar.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { Main } from '../../styles/Navbar';
+import { Main, NavWrap, NavItemList, MenuIcon } from '../../styles/Navbar';
+import {
+	MdAddCircleOutline,
+	MdExitToApp,
+	MdPerson,
+	MdMenu
+} from 'react-icons/md';
+
+const NavItems = [
+	{
+		icon: <MdAddCircleOutline />,
+		name: 'Add Post'
+	},
+	{
+		icon: <MdPerson />,
+		name: 'Profile'
+	},
+	{
+		icon: <MdExitToApp />,
+		name: 'Logout'
+	}
+];
+
+const renderNavItems = () =>
+	NavItems.map((navItem, idx) => {
+		return (
+			<NavItemList key={idx}>
+				<span>{navItem.icon}</span>
+				<p>{navItem.name}</p>
+			</NavItemList>
+		);
+	});
 
 const Navbar = ({ title }) => {
 	return (
 		<Main>
 			<header>
+				<MenuIcon>
+					<MdMenu />
+				</MenuIcon>
 				<h4>{title}</h4>
 			</header>
-			<nav>links</nav>
+			<NavWrap>{renderNavItems()}</NavWrap>
 		</Main>
 	);
 };
--- RD2/writr/client/src/styles/Navbar.js
+++ RD2/writr/client/src/styles/Navbar.js
@@ -13,5 +13,59 @@ export const Main = styled.div`
     color: white;
     font-size: 1.4rem;
   }
+
+  header {
+    display: flex;
+    width: 8rem;
+    align-items: center;
+  }
 `
 
+export const NavWrap = styled.nav`
+  display: flex;
+  width: 14rem;
+  list-style: none;
+  justify-content: space-around;
+  align-items: center;
+`
+
+export const NavItemList = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  transition: .5s ease-in;
+
+  &:hover svg {
+    color: white;
+  }
+
+  p {
+    font-size: 0.6rem;
+    color: #69213a;
+  }
+
+  svg {
+    font-size: 1.58rem;
+    color: #ffbbd2;
+  }
+`
+
+export const MenuIcon = styled.div`
+  margin-right: 1rem;
+  padding-top: 0.4rem;
+
+  svg {
+    font-size: 1.58rem;
+    color: #ffbbd2;
+    transition: .2s ease-in;
+    cursor: pointer;
+
+    &:hover {
+      color: white;
+    }
+  }
+
+  
+`
