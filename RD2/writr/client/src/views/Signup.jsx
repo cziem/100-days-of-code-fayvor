12f03740f86f5bc16660151718ef46808c85b0f0
Fayvor George <phavorsparks@gmail.com>
2019-08-30 22:57:52 +0100

    use formik for login form

--- RD2/writr/client/src/views/Signup.jsx
+++ RD2/writr/client/src/views/Signup.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import {
 	Main,
 	Button,
@@ -10,129 +13,176 @@ import {
 	SelectOption,
 	OptionItem
 } from '../styles/Signup';
-import { validateSignupDetails } from '../helpers/validator';
+import ButtonLoader from '../components/utils/ButtonLoader';
+import Loading from '../components/utils/Loading';
+import Error from '../components/utils/Error';
+import { ADD_NEW_USER } from '../helpers/queries';
 
-const intialState = {
+const SchemaValidation = Yup.object().shape({
+	name: Yup.string().trim(),
+	username: Yup.string()
+		.min(3)
+		.required('username is required'),
+	email: Yup.string()
+		.email('Provide a valid email')
+		.required('email is required'),
+	password: Yup.string()
+		.trim()
+		.required('password is required'),
+	roles: Yup.string()
+});
+
+const initialValues = {
 	name: '',
 	username: '',
 	email: '',
 	password: '',
-	roles: '',
-	user: {}
+	roles: ''
 };
 
-const Signup = () => {
-	const [state, setState] = useState(intialState);
+const Signup = ({ history }) => {
+	const [addNewUser, { loading, error }] = useMutation(ADD_NEW_USER);
 
-	const handleChange = ({ target }) => {
-		const { name, value } = target;
-		setState(prevState => ({ ...prevState, [name]: value }));
-	};
-
-	const handleSelect = ({ target }) =>
-		setState(prevState => ({ ...prevState, roles: target.value }));
-
-	const handleSubmit = e => {
-		e.preventDefault();
-
-		const payload = {
-			name: state.name,
-			username: state.username,
-			email: state.email,
-			password: state.password,
-			roles: state.roles
-		};
-
-		const isValid = validateSignupDetails(payload);
-
-		if (isValid) {
-			console.log('valid');
-		} else {
-			console.log('false');
-		}
-	};
+	if (error)
+		return (
+			<Error ErrorText={error.message.split(':').slice(1)} history={history} />
+		);
+	if (loading) return <Loading />;
 
 	return (
-		<Main>
-			<div className="wrap__main">
-				<h2>get started, writr</h2>
+		<Formik
+			initialValues={initialValues}
+			validationSchema={SchemaValidation}
+			onSubmit={(data, { resetForm, setSubmitting }) => {
+				addNewUser({
+					variables: { ...data }
+				})
+					.then(async ({ data }) => {
+						console.log('data =>', data);
+						setSubmitting(false);
+					})
+					.catch(err => {
+						console.log('Err: ', err);
+						resetForm();
+					});
+			}}
+			render={({
+				values: { name, username, email, password, roles },
+				errors,
+				touched,
+				handleBlur,
+				handleChange,
+				handleSubmit,
+				isSubmitting
+			}) => (
+				<Main>
+					<div className="wrap__main">
+						<h2>get started, writr</h2>
 
-				<Form onSubmit={handleSubmit}>
-					<FormGroup>
-						<Label htmlFor="name">name:</Label>
-						<InputText
-							type="name"
-							placeholder="Jon Snow"
-							name="name"
-							value={state.name}
-							onChange={handleChange}
-							autoFocus
-						/>
-					</FormGroup>
+						<Form onSubmit={handleSubmit}>
+							<FormGroup>
+								<Label htmlFor="name">name:</Label>
+								<InputText
+									type="name"
+									placeholder="Jon Snow"
+									name="name"
+									value={name}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.name && touched.name ? 'error' : ''}
+									autoFocus
+								/>
+								{errors.name && touched.name && (
+									<div className="input-feedback">{errors.name}</div>
+								)}
+							</FormGroup>
 
-					<FormGroup>
-						<Label htmlFor="username">username:</Label>
-						<InputText
-							type="username"
-							placeholder="snow"
-							name="username"
-							value={state.username}
-							onChange={handleChange}
-							required
-						/>
-					</FormGroup>
+							<FormGroup>
+								<Label htmlFor="username">username:</Label>
+								<InputText
+									type="username"
+									placeholder="snow"
+									name="username"
+									value={username}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.username && touched.username ? 'error' : ''}
+									required
+								/>
+								{errors.username && touched.username && (
+									<div className="input-feedback">{errors.username}</div>
+								)}
+							</FormGroup>
 
-					<FormGroup>
-						<Label htmlFor="email">email:</Label>
-						<InputText
-							type="email"
-							placeholder="user@writr.com"
-							name="email"
-							value={state.email}
-							onChange={handleChange}
-							required
-						/>
-					</FormGroup>
+							<FormGroup>
+								<Label htmlFor="email">email:</Label>
+								<InputText
+									type="email"
+									placeholder="user@writr.com"
+									name="email"
+									value={email}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.email && touched.email ? 'error' : ''}
+									required
+								/>
+								{errors.email && touched.email && (
+									<div className="input-feedback">{errors.email}</div>
+								)}
+							</FormGroup>
 
-					<FormGroup>
-						<Label htmlFor="password">password:</Label>
-						<InputText
-							type="password"
-							placeholder="Enter password"
-							name="password"
-							value={state.password}
-							onChange={handleChange}
-							required
-						/>
-					</FormGroup>
+							<FormGroup>
+								<Label htmlFor="password">password:</Label>
+								<InputText
+									type="password"
+									placeholder="Enter password"
+									name="password"
+									value={password}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									className={errors.password && touched.password ? 'error' : ''}
+									required
+								/>
+								{errors.password && touched.password && (
+									<div className="input-feedback">{errors.password}</div>
+								)}
+							</FormGroup>
 
-					<FormGroup>
-						<Label htmlFor="roles">role:</Label>
-						<SelectOption
-							type="roles"
-							placeholder="Enter roles"
-							name="roles"
-							value={state.roles}
-							// onSelect={handleSelect}
-							onChange={handleSelect}
-							required
-						>
-							<OptionItem value={0}>select role</OptionItem>
-							<OptionItem name="editor">editor</OptionItem>
-							<OptionItem name="writer">writer</OptionItem>
-							<OptionItem name="others">others</OptionItem>
-						</SelectOption>
-					</FormGroup>
+							<FormGroup>
+								<Label htmlFor="roles">role:</Label>
+								<SelectOption
+									type="roles"
+									placeholder="Enter roles"
+									name="roles"
+									value={roles}
+									onBlur={handleBlur}
+									onChange={handleChange}
+									className={errors.password && touched.password ? 'error' : ''}
+									required
+								>
+									<OptionItem value={0}>select role</OptionItem>
+									<OptionItem name="editor">editor</OptionItem>
+									<OptionItem name="writer">writer</OptionItem>
+									<OptionItem name="others">others</OptionItem>
+								</SelectOption>
+								{errors.roles && touched.roles && (
+									<div className="input-feedback">{errors.roles}</div>
+								)}
+							</FormGroup>
 
-					<div className="cta">
-						<Button primary>Signup</Button>
-						<Link to="/login">
-							<Button fill="true">login</Button>
-						</Link>
+							<div className="cta">
+								<Button primary>
+									{isSubmitting ? <ButtonLoader /> : 'Signup'}
+								</Button>
+								<Link to="/login">
+									<Button fill="true">login</Button>
+								</Link>
+							</div>
+						</Form>
 					</div>
-				</Form>
-			</div>
-		</Main>
+				</Main>
+			)}
+		/>
 	);
 };
 
